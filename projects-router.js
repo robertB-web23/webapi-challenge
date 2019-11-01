aafd3b33fa18fdce9db47cf95171e692d3d8a58d
Robert Benavidez <murabitbenavidez@gmail.com>
2019-11-01 12:43:34 -0400

    all projects routes and id validator middleware

--- projects-router.js
+++ projects-router.js
@@ -29,5 +29,64 @@ router.get('/:id', (req, res) => {
         })
 });
 
+// POST Project //
+
+router.post('/', (req, res) => {
+    newProject = req.body;
+
+    Projects.insert(newProject)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(error => {
+            res.status(500).json({message:"unable to add project", error})
+        })
+})
+
+// Update project 
+
+router.put('/:id',validateProjectId, (req,res) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    Projects.update(id, changes)
+        .then(project =>{
+            res.status(200).json(project)
+        })
+        .catch(error => {
+            res.status(500).json({ message: "unable to add post"})
+        })
+});
+
+// DELETE Project
+
+router.delete('/:id',validateProjectId, (req, res) => {
+    const id = req.params.id;
+    Projects.remove(id)
+        .then(project => {
+            res.status(200).json({message: "project deleted"})
+        })
+        .catch(error => {
+            res.status(500).json({message: "unable to delete project"})
+        })
+})
+
+// middleware
+
+function validateProjectId(req, res, next) {
+    const id = req.params.id;
+
+    Projects.get(id)
+        .then(project => {
+            if(!project) {
+                return  res.status(400).json({ message: "invalid project id"})
+            }
+        next()
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+};
+
 
 module.exports = router;
