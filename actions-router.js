d78357e39b60a032989a78a34c6509d92e5b93a9
Robert Benavidez <murabitbenavidez@gmail.com>
2019-11-01 13:29:46 -0400

    mvp met

--- actions-router.js
+++ actions-router.js
@@ -29,5 +29,61 @@ router.get('/:id', (req, res) => {
         })
 });
 
+//POST Action
+
+router.post('/',validateProjectExistence, (req, res) => {
+    newAction = req.body;
+    Actions.insert(newAction)
+    .then(action => {
+        res.status(201).json(action)
+    })
+    .catch(error => {
+        res.status(500).json({ message: "unable to add new action" })
+    })
+    }   
+);
+
+//PUT update action
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    Actions.update(id, changes)
+        .then(project =>{
+            res.status(200).json(project)
+        })
+        .catch(error => {
+            res.status(500).json({ message: "unable to add post"})
+        })
+});
+
+//DELETE action
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    Actions.remove(id)
+        .then(action => {
+            res.status(200).json({message: "Action deleted"})
+        })
+        .catch(error => {
+            res.status(500).json({message: "unable to delete action"})
+        })
+})
+
+function validateProjectExistence(req, res, next) {
+    const project = req.body.project_id;
+
+    Projects.get(project)
+        .then(project => {
+            if(!project) {
+                return  res.status(400).json({ message: "invalid project id"})
+            }
+        next()
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+}
+
 
 module.exports = router;
